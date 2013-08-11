6d4d7b5086e4c993353829e9ed46415fea914d69
ChenLong828 <chenlong828@gmail.com>
2013-08-11 21:18:28 +0800

    +)创建基本工程

---
+++ dev_launcher/src/main.go
@@ -0,0 +1,17 @@
+/**
+ * User: ChenLong
+ * Created Date: 8/11/13 5:30 下午
+ * Description: 
+ */
+package main
+
+import "fmt"
+
+var x, y, z int = 1, 2, 3
+var c, python, java = true, false, "no!"
+
+func main() {
+	x = y + z
+	fmt.Println(x)
+	fmt.Print(y)
+}
