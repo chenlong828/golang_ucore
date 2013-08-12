baa6212befbde01e3682f404a7be3e2d8e8e9f4c
ChenLong828 <chenlong828@gmail.com>
2013-08-12 14:32:22 +0800

    +)增加数组、slice样例代码

--- dev_launcher/src/main.go
+++ dev_launcher/src/main.go
@@ -10,8 +10,27 @@ import "fmt"
 var x, y, z int = 1, 2, 3
 var c, python, java = true, false, "no!"
 
-func main() {
+func variable_test() {
 	x = y + z
+	fileName := "hello, world"
 	fmt.Println(x)
-	fmt.Print(y)
+	fmt.Println(y)
+	fmt.Println(fileName)
+}
+
+func array_test() {
+	array := [10]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
+	fmt.Println(array[:5])
+	fmt.Println(len(array))
+
+	var my_slice []byte
+	my_slice = array[2:]
+	fmt.Println(my_slice)
+
+}
+
+func main() {
+
+	variable_test()
+	array_test()
 }
